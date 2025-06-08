366738d8af08184c17af70234c71965eb4daf455
ardie069 <ardi.pulaem069@gmail.com>
2025-06-08 10:33:43 +0700

    feat: add meta titles to routes for improved SEO and user experience

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,18 @@ import Home from "../pages/Home.vue";
 import BlogPost from "../pages/BlogPost.vue";
 
 const routes = [
-  { path: "/", name: "Home", component: Home },
-  { path: "/post", name: "BlogPost", component: BlogPost },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    meta: { title: "HMIF SMI Malang" },
+  },
+  {
+    path: "/post",
+    name: "BlogPost",
+    component: BlogPost,
+    meta: { title: "HMIF SMI Malang | Blog" },
+  },
 ];
 
 const router = createRouter({
@@ -19,6 +29,14 @@ const router = createRouter({
     }
     return { top: 0 };
   },
+});
+
+router.afterEach((to) => {
+  if (to.meta && to.meta.title) {
+    document.title = to.meta.title;
+  } else {
+    document.title = "HMIF SMI Malang";
+  }
 });
 
 export default router;
