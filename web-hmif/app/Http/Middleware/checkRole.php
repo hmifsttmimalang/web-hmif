682411c22ed71c37fb33b9663e2e634445567e73
Ardiansyah <ardi.pulaem069@gmail.com>
2025-06-23 19:35:06 +0700

    feat: implement role-based access control middleware for admin and superadmin

--- web-hmif/app/Http/Middleware/checkRole.php
+++ web-hmif/app/Http/Middleware/checkRole.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class AdminOnly
+class checkRole
 {
     /**
      * Handle an incoming request.
@@ -16,10 +16,10 @@ class AdminOnly
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (!Auth::check() || Auth::user()->role !== 'admin') {
+        if (!Auth::check() || !in_array(Auth::user()->role, ['admin', 'superadmin'])) {
             return redirect()->route('unauthorized')->with('error', 'Akses khusus admin!');
         }
-        
+
         return $next($request);
     }
 }
