8f6ce4e1de72e11661c109e714f79d62b61da431
Ardiansyah <ardi.pulaem069@gmail.com>
2025-06-26 13:36:35 +0700

    feat: add Head component for page title in KelolaData.vue

--- resources/js/Pages/Admin/KelolaData.vue
+++ resources/js/Pages/Admin/KelolaData.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { Head } from "@inertiajs/vue3";
 import DashAdminLayout from '@/layouts/DashAdminLayout.vue';
 import KelolaDataContent from '@/components/admin/kelola-data/KelolaDataContent.vue';
 </script>
 
 <template>
+    <Head title="Kelola Data" />
     <DashAdminLayout>
         <KelolaDataContent />
     </DashAdminLayout>
