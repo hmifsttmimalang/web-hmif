e0584182de5996a37b97c890a0f7bb2c1487f487
Ardiansyah <ardi.pulaem069@gmail.com>
2025-06-30 21:22:27 +0700

    feat: add sweetalert2 dependency and clean up import statements in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
-import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import './bootstrap'
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import './bootstrap';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
