60226963ec19e792fcaa4f56b6acbb350f0a5eb3
ardie069 <ardi.pulaem069@gmail.com>
2025-07-06 13:44:26 +0700

    feat: refactor axios configuration and CSRF handling in bootstrap.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,41 +2,11 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import './bootstrap';
 
-import axios from 'axios';
-axios.defaults.withCredentials = true;
-axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const token = document.head.querySelector('meta[name="csrf-token"]');
-if (token) {
-    axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-} else {
-    console.error('CSRF token not found!');
-}
-
-axios.interceptors.request.use(config => {
-    const token = getCookieValue('XSRF-TOKEN');
-    if (token) {
-        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(token);
-    }
-    return config;
-});
-
-function getCookieValue(name) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i].trim();
-        if (cookie.startsWith(name + '=')) {
-            return cookie.substring(name.length + 1);
-        }
-    }
-    return null;
-}
-
+import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "aos/dist/aos.css";
-import "bootstrap";
 import "../css/app.css";
 
 createInertiaApp({
@@ -46,18 +16,17 @@ createInertiaApp({
         return page && page.default ? page.default : page;
     },
     setup({ el, App, props, plugin }) {
-      const vueApp = createApp({ render: () => h(App, props) })
-                .use(plugin);
-      
-      import('aos').then(AOS => {
-        AOS.init();
-      });
-      
-      import('@srexi/purecounterjs').then(({ default: PureCounter }) => {
-        new PureCounter();
-      });
-      
-      vueApp.mount(el);
+        const vueApp = createApp({ render: () => h(App, props) })
+            .use(plugin);
+
+        import('aos').then(AOS => {
+            AOS.init();
+        });
+
+        import('@srexi/purecounterjs').then(({ default: PureCounter }) => {
+            new PureCounter();
+        });
+
+        vueApp.mount(el);
     },
 });
-
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,32 @@
 import axios from 'axios';
 window.axios = axios;
 
+window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found!');
+}
+
+window.axios.interceptors.request.use(config => {
+    const getCookieValue = (name) => {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.startsWith(name + '=')) {
+                return cookie.substring(name.length + 1);
+            }
+        }
+        return null;
+    };
+
+    const xsrf = getCookieValue('XSRF-TOKEN');
+    if (xsrf) {
+        config.headers['X-XSRF-TOKEN'] = decodeURIComponent(xsrf);
+    }
+
+    return config;
+});
